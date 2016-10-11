9222c7fdc93b35321a6e222d957a05bc22654f6a
macharianyagah <petermacharia26@gmail.com>
2016-10-11 06:32:07 +0300

    Add files via upload

---
+++ data_type.py
@@ -0,0 +1,28 @@
+def data_type(data):
+
+  if type(data) == str:
+    return len(data)
+
+  elif data == None:
+    return "no value"
+
+  elif data == True or data == False:
+    return data
+
+  elif type(data) == int:
+
+    if data < 100:
+      return "less than 100"
+
+    elif data > 100:
+      return "more than 100"
+
+    else:
+      return "equal to 100"
+
+  elif type(data) == list:
+
+    if len(data) >= 3:
+      return data[2]
+    else:
+      return None
